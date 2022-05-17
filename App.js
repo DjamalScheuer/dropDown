f2bcaf81b310c7ef9180e8530c5d18d94cf3209b
Djamal Scheuer <djamal@trollgames.de>
2022-05-17 18:27:26 +0200

    first DropDown

--- App.js
+++ App.js
@@ -1,21 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useMemo, useEffect } from 'react';
+
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Picker, onOpen } from 'react-native-actions-sheet-picker';
+
+/*
+ **Example data:
+ */
+import countries from './countries.json';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [data, setData] = useState([]);
+    const [selected, setSelected] = useState(undefined);
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        setData(countries);
+    }, []);
+
+    /*
+     **Example filter function
+     * @param {string} filter
+     */
+    const filteredData = useMemo(() => {
+        if (data && data.length > 0) {
+            return data.filter((item) =>
+                item.name
+                    .toLocaleLowerCase('en')
+                    .includes(query.toLocaleLowerCase('en'))
+            );
+        }
+    }, [data, query]);
+
+    /*
+     **Input search
+     *@param {string} text
+     */
+    const onSearch = (text) => {
+        setQuery(text);
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    onOpen('praxis');
+                }}
+            >
+                <Text style={styles.text}>Praxis auswählen</Text>
+            </TouchableOpacity>
+            <Text style={{ padding: 10 }}>Aktuelle Praxis: {JSON.stringify(selected)}</Text>
+            <Picker
+                id="praxis"
+                data={filteredData}
+                inputValue={query}
+                searchable={true}
+                label="Wähle deine Praxis!"
+                setSelected={setSelected}
+                onSearch={onSearch}
+            />
+
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    onOpen('country');
+                }}
+            >
+                <Text style={styles.text}>Behandler auswählen</Text>
+            </TouchableOpacity>
+            <Text style={{ padding: 10 }}>Aktueller Behandler: {JSON.stringify(selected)}</Text>
+            <Picker
+                id="country"
+                data={filteredData}
+                inputValue={query}
+                searchable={true}
+                label="Wähle deinen Behandler!"
+                setSelected={setSelected}
+                onSearch={onSearch}
+            />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: '#0ebbff',
+        padding: 10,
+        borderRadius: 6,
+        marginTop: 50,
+    },
+    text: {
+        color: '#ffffff',
+    },
 });
